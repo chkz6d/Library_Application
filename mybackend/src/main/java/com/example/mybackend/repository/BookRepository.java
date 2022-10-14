36a261cba47cd1d3c7bd5029e218ae38ff027070
Chan Kim <60370096+chkz6d@users.noreply.github.com>
2022-10-14 11:20:06 -0400

    Delete mybackend directory

--- mybackend/src/main/java/com/example/mybackend/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mybackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mybackend.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer>{
-
-}
--- mybackend/src/main/java/com/example/mybackend/repository/LibraryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mybackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mybackend.model.Library;
-
-public interface LibraryRepository extends JpaRepository<Library, Integer>{
-
-}
